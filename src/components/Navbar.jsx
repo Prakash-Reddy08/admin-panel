1e9819985b67baa48f20d2a81f8d4f4a31de97fe
Prakash-Reddy08 <mjprakashreddy1@gmail.com>
2022-05-09 11:10:21 +0530

    create navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,132 @@
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import LanguageOutlinedIcon from '@mui/icons-material/LanguageOutlined';
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
+import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
+import styled from 'styled-components';
+
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <Wrapper>
+            <div className='navbar'>
+                <div className="wrap">
+                    <div className="search">
+                        <input type="text" id='search' placeholder="Search..." />
+                        <label htmlFor="search">
+                            <SearchOutlinedIcon />
+                        </label>
+                    </div>
+                    <div className="items">
+                        <div className="item">
+                            <LanguageOutlinedIcon className='icon' />
+                        </div>
+                        <div className="item">
+                            <DarkModeOutlinedIcon
+                                className="icon"
+                            />
+                        </div>
+                        <div className="item">
+                            <FullscreenExitOutlinedIcon className="icon" />
+                        </div>
+                        <div className="item">
+                            <NotificationsNoneOutlinedIcon className="icon" />
+                            <div className="counter">1</div>
+                        </div>
+                        <div className="item">
+                            <ChatBubbleOutlineOutlinedIcon className="icon" />
+                            <div className="counter">2</div>
+                        </div>
+                        <div className="item">
+                            <ListOutlinedIcon className="icon" />
+                        </div>
+                        <div className="item">
+                            <img
+                                src="https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+                                alt=""
+                                className="avatar"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Wrapper>
     )
 }
+
+const Wrapper = styled.div`
+.navbar{
+        height: 50px;
+        border-bottom: 0.5px solid rgb(231, 228, 228);
+        display: flex;
+        align-items: center;
+        font-size: 14px;
+        color: #555;
+        .wrap{
+            width: 100%;
+            padding: 20px;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            .search {
+                display: flex;
+                align-items: center;
+                border: 0.5px solid lightgray;
+                padding: 3px;
+                label{
+                    display:flex;
+                }
+                input {
+                    border: none;
+                    outline: none;
+                    background: transparent;
+
+                    &::placeholder {
+                    font-size: 12px;
+                    }
+                }
+            }
+            .items {
+                display: flex;
+                align-items: center;
+
+                .item {
+                    display: flex;
+                    align-items: center;
+                    margin-right: 20px;
+                    position: relative;
+
+                    .icon {
+                    font-size: 20px;
+                    }
+
+                    .avatar {
+                    width: 30px;
+                    height: 30px;
+                    border-radius: 50%;
+                    }
+
+                    .counter {
+                    width: 15px;
+                    height: 15px;
+                    background-color: red;
+                    border-radius: 50%;
+                    color: white;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    font-size: 10px;
+                    font-weight: bold;
+                    position: absolute;
+                    top: -5px;
+                    right: -5px;
+                    }
+                }
+            }
+        }
+        
+    }
+`
 
 export default Navbar
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import Navbar from '../../components/Navbar';
 import Sidebar from '../../components/Sidebar';
 
 const Home = () => {
@@ -6,7 +7,10 @@ const Home = () => {
         <Wrapper>
             <div className='home'>
                 <Sidebar />
-                <div className="homeContainer">Container</div>
+                <div className="homeContainer">
+                    <Navbar />
+                    Home content
+                </div>
             </div>
         </Wrapper>
     )
