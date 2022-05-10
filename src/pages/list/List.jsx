5d0c10ff0b47f3c2202063d98e12dea91e01cbd9
Prakash-Reddy08 <mjprakashreddy1@gmail.com>
2022-05-10 13:16:59 +0530

    create users list

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,11 +1,29 @@
 import styled from 'styled-components';
+import Datatable from '../../components/Datatable';
+import Navbar from '../../components/Navbar';
+import Sidebar from '../../components/Sidebar';
 
 const List = () => {
     return (
-        <div>List</div>
+        <Wrapper>
+
+            <div className="list">
+                <Sidebar />
+                <div className="listContainer">
+                    <Navbar />
+                    <Datatable />
+                </div>
+            </div>
+        </Wrapper>
     )
 }
 const Wrapper = styled.div`
-
+.list{
+  display: flex;
+  width: 100%;
+  .listContainer {
+    flex: 6;
+  }
+}
 `
 export default List
